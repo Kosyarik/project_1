9e9fdd125ca777be92d52dacd256e3d08183b730
Yarik <81637871+Kosyarik@users.noreply.github.com>
2021-04-25 21:04:12 +0300

    Add files via upload

--- js/carusel.js
+++ js/carusel.js
@@ -1,20 +1,24 @@
-const buttonsWrapper = document.querySelector(".map");
-const slides = document.querySelector(".inner");
-
-buttonsWrapper.addEventListener("click", e => {
-	if (e.target.nodeName === "BUTTON") {
-		Array.from(buttonsWrapper.children).forEach(item =>
-			item.classList.remove("active")
-		);
-		if (e.target.classList.contains("first")) {
-			slides.style.transform = "translateX(-0%)";
-			e.target.classList.add("active");
-		} else if (e.target.classList.contains("second")) {
-			slides.style.transform = "translateX(-33.33333333333333%)";
-			e.target.classList.add("active");
-		} else if (e.target.classList.contains('third')) {
-			slides.style.transform = 'translatex(-66.6666666667%)';
-			e.target.classList.add('active');
-		}
-	}
+$(document).ready(function () {
+	$('.slider').slick({
+		arrows: true,
+		slidesToShow: 3,
+		autoplay: true,
+		speed: 1000,
+		autoplaySpeed: 800,
+		responsive: [
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 2
+				}
+			},
+			{
+				breakpoint: 550,
+				settings: {
+					slidesToShow: 1
+				}
+			}
+		]
+	});
 });
+
